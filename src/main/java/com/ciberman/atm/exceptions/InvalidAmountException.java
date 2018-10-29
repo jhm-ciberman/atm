95b40ba4ca544dbc8796d08283116b7646592634
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2018-10-29 07:55:06 -0300

    Clean code

--- src/main/java/com/ciberman/atm/exceptions/InvalidAmountException.java
+++ src/main/java/com/ciberman/atm/exceptions/InvalidAmountException.java
@@ -1,19 +1,8 @@
 package com.ciberman.atm.exceptions;
 
-import com.ciberman.atm.views.BaseView;
-import com.ciberman.atm.views.MainMenuView;
-
 public class InvalidAmountException extends ATMError {
 
-    private Class<? extends BaseView> redirectTo = MainMenuView.class;
-
-    public InvalidAmountException(Class<? extends BaseView> redirectTo) {
+    public InvalidAmountException() {
         super("El monto introducido no es válido", "Intente nuevamente");
-        this.redirectTo = redirectTo;
     }
-
-    public void setRedirectTo(Class<? extends BaseView> redirectTo) {
-        this.redirectTo = redirectTo;
-    }
-
 }
